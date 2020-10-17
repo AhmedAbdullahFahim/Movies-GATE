f0f008a7e330f70ca89fc57c40a689564c59847f
AhmedAbdullahFahim <ahmed.abdallah5022@gmail.com>
2020-10-18 02:03:34 +0300

    Production build

---
+++ build/precache-manifest.2b93bb67cfb00e63c1d04d4743ee7574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c770a5ef12fe2263e732e5bd130a5bc0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bcfafb0204b83cde255c",
+    "url": "./static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "82b4c6da323224c0d53c",
+    "url": "./static/css/main.31a501c5.chunk.css"
+  },
+  {
+    "revision": "bcfafb0204b83cde255c",
+    "url": "./static/js/2.d2f99785.chunk.js"
+  },
+  {
+    "revision": "8aa5ce1e30c532c6aca185a23a60927f",
+    "url": "./static/js/2.d2f99785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b4c6da323224c0d53c",
+    "url": "./static/js/main.088422c2.chunk.js"
+  },
+  {
+    "revision": "383ed8c4979ed2dfede6",
+    "url": "./static/js/runtime-main.4040f97b.js"
+  }
+]);
